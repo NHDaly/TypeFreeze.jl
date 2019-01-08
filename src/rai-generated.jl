1736fb859804bd8624dd2a88a189cf2afd2d6f09
Nathan Daly <nhdaly@gmail.com>
2019-01-08 14:05:42 -0500

    Fix `@rAI_generated` to also call the newly defined function at the end.

--- src/rai-generated.jl
+++ src/rai-generated.jl
@@ -3,16 +3,22 @@ using InteractiveUtils
 
 include("typefreeze.jl")
 
-@generated function bar(x)
-    if x <: Integer
-        return :(x ^ 2)
-    else
-        return :(x)
-    end
-end
+#@generated function bar(x)
+#    if x <: Integer
+#        return :(x ^ 2)
+#    else
+#        return :(x)
+#    end
+#end
+#
+#bar(4)
 
-bar(4)
+"""
+    @rAI_generated function f(x) :(x+1) end
 
+Implements the same functionality as @generated, but using `@eval`, so that it is guaranteed
+to generate a new function for given type parameters exactly once.
+"""
 macro rAI_generated(f)
     if isa(f, Expr) && (f.head === :function || Base.is_short_function_def(f))
         body = f.args[2]
@@ -23,13 +29,33 @@ macro rAI_generated(f)
     end
 end
 
+function raw_argnames(args::Array)
+    tmpcount = 0
+    out = []
+    for a in args
+        name = raw_argname(a)
+        if name == nothing
+            tmpcount += 1
+            name = Symbol("_$tmpcount")
+        end
+        push!(out, name)
+    end
+    out
+end
+raw_argname(x::Symbol) = x
+function raw_argname(e::Expr)
+    @assert e.head == Symbol("::")
+    return length(e.args) == 2 ? e.args[1] : nothing
+end
+
 function gen_eval_f(funcexpr::Expr)
     dump(funcexpr)
     name = funcexpr.args[1].args[1]
     escname = esc(name)
     args = funcexpr.args[1].args[2:end]
     types = argtypes(args)
-    names = argnames(args)
+    names = raw_argnames(args)
+    esc_argnames = argnames(args)
     function_params = paramexprs(args)
     escargs = [esc(a) for a in args]
     body = esc(funcexpr.args[2])
@@ -38,24 +64,30 @@ function gen_eval_f(funcexpr::Expr)
             $body
         end
         function $escname($(function_params...))
+            names = $names
             types = [$(types...)]
-            val = funchelper(types...)
-            typedargs = [Expr(Symbol("::"), names[i], types[i]) for i in 1:length(args)]
+            generated_body = funchelper(types...)
+            typedargs = [Expr(Symbol("::"), names[i], types[i]) for i in 1:$(length(args))]
             #@eval $finalcall = $(Expr(:$, :val))
-            @eval $name($(Expr(:$, :(typedargs...)))) = $(Expr(:$, :val))
-            return val
+            @eval $name($(Expr(:$, :(typedargs...)))) = $(Expr(:$, :generated_body))
+            return @eval $name($([Expr(:$, n) for n in names]...))
         end
     end
 end
 gen_eval_f(:(f(x::Int64) = 3))
 
-@rAI_generated function baz(x)
+@rAI_generated function bar(x)
     if x <: Integer
         return :(x ^ 2)
     else
         return :(x)
     end
 end
-@code_typed baz(3)
+@show methods(bar)
+@show bar(3.0)
+@show methods(bar)
+@show bar(3.0)
+@show bar(3)
+@code_typed bar(3.0)
 
 end
