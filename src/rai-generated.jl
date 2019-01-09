e7f86608bbca964d22dd03d82def24283235b55d
Nathan Daly <nhdaly@gmail.com>
2019-01-08 23:26:32 -0500

    Added notes from talking to jrevels

--- src/rai-generated.jl
+++ src/rai-generated.jl
@@ -3,16 +3,6 @@ using InteractiveUtils
 
 include("typefreeze.jl")
 
-#@generated function bar(x)
-#    if x <: Integer
-#        return :(x ^ 2)
-#    else
-#        return :(x)
-#    end
-#end
-#
-#bar(4)
-
 """
     @rAI_generated function f(x) :(x+1) end
 
@@ -65,7 +55,7 @@ function gen_eval_f(funcexpr::Expr)
         end
         function $escname($(function_params...))
             names = $names
-            types = [$(types...)]
+            types = [$((:typeof(x) for x in params)...)]
             generated_body = funchelper(types...)
             typedargs = [Expr(Symbol("::"), names[i], types[i]) for i in 1:$(length(args))]
             #@eval $finalcall = $(Expr(:$, :val))
@@ -76,6 +66,19 @@ function gen_eval_f(funcexpr::Expr)
 end
 gen_eval_f(:(f(x::Int64) = 3))
 
+# EXAMPLE:
+# This is what the macro expands to:
+function bar(x)
+    names = [:x]
+    types = [typeof(x)]
+    gen_body = bar_helper(types...)
+    # Real @generated creates method instances, not methods
+    #  Consequence: allows you to redefine body of bar() in the future.
+    # Also: slower! Dynamic dispatch is slow, and gets slower w/ more methods
+    @eval bar(x::types...) = $(gen_body)
+    return @eval bar(x)
+end
+
 @rAI_generated function bar(x)
     if x <: Integer
         return :(x ^ 2)
@@ -83,11 +86,29 @@ gen_eval_f(:(f(x::Int64) = 3))
         return :(x)
     end
 end
+
+# Generated functions in general:
+#  - dynamic dispatch, more often, and with more methodinstances
+#  - Forced to have one methodinstances per unique params type tuple
+#  - Forced to know the runtime types always to have statically inferrable code
+
 @show methods(bar)
 @show bar(3.0)
 @show methods(bar)
 @show bar(3.0)
 @show bar(3)
 @code_typed bar(3.0)
+
+# Compare with the built-in @generated, which doesn't add new methods:
+# @generated function baz(x)
+#     if x <: Integer
+#         return :(x ^ 2)
+#     else
+#         return :(x)
+#     end
+# end
+#
+# @show methods(baz)
+# baz(4)
 
 end
